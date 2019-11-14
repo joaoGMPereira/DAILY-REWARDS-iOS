a72682fc5d6962e7a8647b78607225f6a5b14e93
jgmolbc <joao.perei@itau-unibanco.com.br>
2019-11-14 09:44:00 -0300

    Initial Commit

---
+++ DailyRewards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyRewards
+//
+//  Created by Joao Gabriel Medeiros Perei on 14/11/19.
+//  Copyright © 2019 Joao Gabriel Medeiros Perei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DailyRewardsTests/DailyRewardsTests.swift
@@ -0,0 +1,34 @@
+//
+//  DailyRewardsTests.swift
+//  DailyRewardsTests
+//
+//  Created by Joao Gabriel Medeiros Perei on 14/11/19.
+//  Copyright © 2019 Joao Gabriel Medeiros Perei. All rights reserved.
+//
+
+import XCTest
+@testable import DailyRewards
+
+class DailyRewardsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
