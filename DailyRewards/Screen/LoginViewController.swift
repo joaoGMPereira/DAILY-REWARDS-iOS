49937925e14e876b1a7263cca6fc95fa2d27f33b
jgmolbc <joao.perei@itau-unibanco.com.br>
2019-11-22 20:05:21 -0300

    ajustes para inicializar o projeto

--- DailyRewards/Screen/LoginViewController.swift
+++ DailyRewards/Screen/LoginViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Firebase
 import GoogleSignIn
 import Lottie
+import JewFeatures
 
 class LoginViewController: UIViewController {
 
@@ -23,12 +24,31 @@ class LoginViewController: UIViewController {
         signInButton.colorScheme = .dark
         signInButton.style = .wide
         
-        let loadAnimation = Animation.named("IconLottie")
+        let loadAnimation = Animation.named("IconLottie", bundle: Bundle(for: type(of: self)))
         iconLottieView.animation = loadAnimation
         iconLottieView.contentMode = .scaleAspectFit
         iconLottieView.animationSpeed = 1.0
         iconLottieView.play(fromFrame: AnimationFrameTime(89), toFrame: AnimationFrameTime(89), loopMode: .playOnce, completion: nil)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let teste = JEWPresentBaseViewController()
+        self.present(teste, animated: true) {
+            teste.showLoading()
+        }
+//        let errorViewController = JEWConnectorHelpers.setupAlertController(lastViewController: self, message: "OLA Teste", title: "Title Test")
+//        errorViewController.confirmCallback = { (button) -> () in
+//            Timer.init(timeInterval: 3.0, repeats: false, block: { (timer) in
+//                errorViewController.dismiss(animated: true) {
+//                }
+//            })
+//
+//        }
+//        let teste = JEWPopupMessage.init(parentViewController: self)
+//        teste.show(withTextMessage: "Teste")
+//        teste.show(withAttributedMessage: "testando", title: "O teste", popupType: .alert, shouldHideAutomatically: false)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
