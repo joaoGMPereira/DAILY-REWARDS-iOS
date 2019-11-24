ae01f30ef10378622d4a2cabc519d0a98f95e88c
jgmolbc <joao.perei@itau-unibanco.com.br>
2019-11-23 22:38:49 -0300

    removing pods

--- Pods/Target Support Files/Pods-DailyRewards/Pods-DailyRewards-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DailyRewards : NSObject
-@end
-@implementation PodsDummy_Pods_DailyRewards
-@end
--- Pods/Target Support Files/StepView/StepView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_StepView : NSObject
-@end
-@implementation PodsDummy_StepView
-@end
